c4fcad9dd1295e90c5f4232c347438e143564fe0
Gabriela Martinez <gmartinezsan@gmail.com>
2020-02-12 22:22:20 -0600

    Adding reversestring exercise

---
+++ reversestring/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace reversestring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           char[] word = new char[]{'h','e','l','l','o'}; 
+           ReverseString(word);
+           foreach (var c in word)
+           {
+              Console.Write(c);               
+           }
+        }
+
+        public static void ReverseString(char[] s)
+        {
+            int i = s.Length - 1;
+            int j = 0;
+            char temp;
+            while (i >= j)
+            {
+                temp = s[j];
+                s[j] = s[i];
+                s[i] = temp;
+                j++;
+                i--;
+            }
+        }
+    }
+}
