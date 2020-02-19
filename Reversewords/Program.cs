154d7c7b6f56f36c7ccbd9fd3a78c83926d450de
Gabriela Martinez <gmartinezsan@gmail.com>
2020-02-18 23:10:24 -0600

    Daily practice

---
+++ Reversewords/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+// Your input
+// "the sky is blue"
+// Expected
+// "blue is sky the"
+
+namespace Reversewords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("the sky is blue"));
+        }
+
+        public static string ReverseWords(string s) {
+        
+        if (s == null || s.Length == 0 )
+            return string.Empty;
+        
+         string[] splitted_s = s.Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries);        
+         Collection<string> c = new Collection<string>(splitted_s);        
+         Stack<string> words = new Stack<string>(c);
+        
+        return string.Join(' ', words.ToArray());       
+    }
+    }
+}
